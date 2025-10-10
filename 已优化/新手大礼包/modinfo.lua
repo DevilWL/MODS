94304276a4903541242c4e47303b77221c3cba37
DevilWL <qq422909800@163.com>
2025-10-11 02:08:57 +0800

    调整

---
+++ 已优化/新手大礼包/modinfo.lua
@@ -0,0 +1,262 @@
+name = "新手大礼包"
+description = "堆叠上限，快速采集，冰箱加强保鲜，角色移动加快，自动回血等"
+author = "Devil"
+version = "1.1.0"
+
+forumthread = ""
+
+--DLC支持
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+--模组API版本 单机版：6
+api_version = 6
+
+--模组图标
+icon_atlas = ""
+icon = ""
+
+--模组配置
+configuration_options = 
+{
+
+	--堆叠上限
+
+	{
+		name = "text",
+		label = "全部三种堆叠类型",
+		options = 
+		{
+			{description = "数量上限修改", data = false},
+
+		},
+		default = false
+	},
+	{
+		name = "stack_size1",
+		label = "小型堆叠：树枝等",
+		options = 
+		{
+			{description = "50", data = 50},
+			{description = "100", data = 100},
+			{description = "200", data = 200},
+			{description = "500", data = 500},
+			{description = "800", data = 800},
+			{description = "999", data = 999},
+		},
+		default = 200
+	},
+	{
+		name = "stack_size2",
+		label = "中型堆叠：木头等",
+		options = 
+		{
+			{description = "50", data = 50},
+			{description = "100", data = 100},
+			{description = "200", data = 200},
+			{description = "500", data = 500},
+			{description = "800", data = 800},
+			{description = "999", data = 999},
+		},
+		default = 100
+	},
+	{
+		name = "stack_size3",
+		label = "大型堆叠：木板等",
+		options = 
+		{
+			{description = "50", data = 50},
+			{description = "100", data = 100},
+			{description = "200", data = 200},
+			{description = "500", data = 500},
+			{description = "800", data = 800},
+			{description = "999", data = 999},
+		},
+		default = 50
+	},
+	
+	--移动速度介绍
+	--[[
+	{
+		name = "text2",
+		label = "移动速度保持",
+		options = 
+		{
+			{description = "步行跑步同步", data = false},
+
+		},
+		default = false
+	},
+	]]
+		--移动速度开关
+	{
+		name = "WalkRun",
+		label = "是否开启角色加速",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+		},
+		default = false
+	},
+	
+	
+	
+	
+	
+	
+	
+	{
+		name = "walk_speed",
+		label = "步行速度建议同步",
+		options = 
+		{
+			{description = "不变", data = 4},
+			{description = "1.5倍", data = 6},
+			{description = "2倍", data = 8},
+			{description = "2.5倍", data = 10},
+			{description = "3倍", data = 12},
+		},
+		default = 6
+	},
+	{
+		name = "run_speed",
+		label = "跑步速度建议同步",
+		options = 
+		{
+			{description = "不变", data = 6},
+			{description = "1.5倍", data = 9},
+			{description = "2倍", data = 12},
+			{description = "2.5倍", data = 15},
+			{description = "3倍", data = 18},
+		},
+		default = 9
+	},
+	--后续考虑是否改成1的数值递增
+	
+	--冰箱保鲜程度
+		{
+		name = "backpackfridge",
+		label = "是否全部背包保鲜",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+
+		},
+		default = false
+	},
+		{
+		name = "Playerfridge",
+		label = "角色自带格子保鲜",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+		},
+		default = false
+	},
+	
+	
+	
+	{
+		name = "icebox_freeze",
+		label = "所有种类保鲜效果",
+		options = 
+		{
+			{description = "默认", data = .5},
+			{description = "4倍", data = .25},
+			{description = "5倍", data = .2},
+			{description = "永久", data = 0},
+			{description = "回鲜", data = -5},
+		},
+		default = 0.25
+	},
+	
+
+	--[[
+		{
+		name = "attack",
+		label = "攻击力",
+		options = 
+		{
+			{description = "默认", data = 34},
+			{description = "2倍", data = 68},
+			{description = "3倍", data =112},
+		},
+		default = 68
+	},
+	]]
+	
+	{
+		name = "MOD_QuickPick",
+		label = "是否开启快速采集",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+		},
+		default = true
+	},
+	
+	
+	{
+		name = "healthregen",
+		label = "是否开启角色回血",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+		},
+		default = false
+	},
+	
+	
+	{
+		name = "regen_time",
+		label = "角色每次回血间隔",
+		options = 
+		{
+			{description = "1秒", data = 1},
+			{description = "3秒", data = 3},
+			{description = "5秒", data = 5},
+			{description = "10秒", data = 10},
+		},
+		default = 5
+	},
+	
+	
+	
+	{
+		name = "regen_health",
+		label = "角色每次回复血量",
+		options = 
+		{
+			{description = "1", data = 1},
+			{description = "3", data = 3},
+			{description = "5", data = 5},
+			{description = "10", data = 10},
+			{description = "100", data = 100},
+			{description = "250", data = 250},
+			{description = "500", data = 500},
+			{description = "999", data = 999},
+		},
+		default = 1
+	},
+
+	{
+		name = "wathgrithr_vegetarian",
+		label = "女武神吃素",
+		options = 
+		{
+			{description = "关闭", data = false},
+			{description = "开启", data = true},
+		},
+		default = false
+	}
+	
+	
+	
+}
+
---
+++ 已优化/新手大礼包/modmain.lua
@@ -0,0 +1,204 @@
+
+---------------------------------------------------------------
+------------------------------堆叠上限-------------------------
+---------------------------------------------------------------
+
+-- 设置自定义堆叠函数
+function stackablepostinit(inst)
+	inst:AddComponent("stackable")
+	inst.components.stackable.maxsize = GetModConfigData("stack_size2")
+end
+
+-- 单独设置金币与暗影燃料堆叠函数
+function stackabledubloonnightmarefuel(inst) --函数名可自行更换
+	inst:AddComponent("stackable")
+	inst.components.stackable.maxsize = 999999
+end
+
+-- 堆叠上限修改
+TUNING.STACK_SIZE_LARGEITEM = GetModConfigData("stack_size3")
+TUNING.STACK_SIZE_MEDITEM = GetModConfigData("stack_size2")
+TUNING.STACK_SIZE_SMALLITEM = GetModConfigData("stack_size1")
+
+-- 通过API接口使某些不可堆叠物品变成可堆叠
+AddPrefabPostInit("rabbit", stackablepostinit)        		 --兔子
+AddPrefabPostInit("robin", stackablepostinit)         		 --鸟
+AddPrefabPostInit("robin_winter", stackablepostinit) 		 --雪鸟
+AddPrefabPostInit("crow", stackablepostinit)         		 --乌鸦
+AddPrefabPostInit("parrot", stackablepostinit)      		 --鹦鹉
+AddPrefabPostInit("seagull", stackablepostinit)     		 --海鸥
+AddPrefabPostInit("toucan", stackablepostinit)     		     --巨嘴鸟
+AddPrefabPostInit("crab", stackablepostinit)        		 --螃蟹
+AddPrefabPostInit("jellyfish", stackablepostinit)    		 --水母
+AddPrefabPostInit("snakeoil", stackablepostinit)     		 --蛇油
+AddPrefabPostInit("coral_brain", stackablepostinit)  		 --珊瑚脑
+AddPrefabPostInit("lobster", stackablepostinit)      		 --龙虾
+AddPrefabPostInit("minotaurhorn", stackablepostinit) 		 --犀牛角
+AddPrefabPostInit("tallbirdegg", stackablepostinit)  		 --高鸟蛋
+AddPrefabPostInit("doydoy", stackablepostinit)       		 --渡渡鸟
+AddPrefabPostInit("doydoybaby", stackablepostinit)   		 --渡渡鸟宝宝
+AddPrefabPostInit("magic_seal", stackablepostinit)   		 --海豹纹章
+AddPrefabPostInit("doydoyegg", stackablepostinit)    		 --渡渡鸟蛋
+AddPrefabPostInit("coconade", stackablepostinit)     		 --椰子炸弹
+AddPrefabPostInit("obsidiancoconade", stackablepostinit)	 --黑曜石炸弹
+AddPrefabPostInit("beemine", stackablepostinit)     		 --蜜蜂地雷
+AddPrefabPostInit("quackenbeak", stackablepostinit)   		 --海妖喙
+
+-- 单独设置金币和暗影燃料的数量，方便将来配合RPG类型模组使用
+AddPrefabPostInit("dubloon", stackabledubloonnightmarefuel)
+AddPrefabPostInit("nightmarefuel", stackabledubloonnightmarefuel)
+
+---------------------------------------------------------------
+------------------------------冰箱保鲜-------------------------
+---------------------------------------------------------------
+
+-- 角色自带格子保鲜，也会同时作用于各种背包
+
+local Playerfridge = GetModConfigData("Playerfridge")
+
+if Playerfridge == true then
+AddPlayerPostInit(function(inst)
+inst:AddTag("fridge")
+end)
+end
+
+
+local backpackfridge = GetModConfigData("backpackfridge")
+
+local function OnPickup(inst, owner)
+    -- 检查玩家是否已经有一个背包
+    local hasBackpack = false
+    for k, item in pairs(owner.components.inventory:GetItems()) do
+        if item.prefab == "backpack" or item.prefab == "your_backpack_prefab_name" then
+            hasBackpack = true
+            break
+        end
+    end
+
+    -- 如果已经有背包，则不允许捡起新的背包
+    if hasBackpack then
+        owner.components.inventory:DropItem(inst) -- 强制丢弃新捡起的背包
+        -- 可以添加提示信息
+        if owner.components.talker then
+            owner.components.talker:Say("我只能携带一个背包！")
+        end
+        return
+    end
+
+    -- 正常捡起背包的逻辑
+    inst.components.inventoryitem.cangoincontainer = false
+end
+
+
+local function fridgebackpack(inst)
+	inst:AddComponent("inspectable")
+    inst:AddTag("fridge")
+end
+
+if backpackfridge == true then
+	AddPrefabPostInit("backpack", fridgebackpack)
+	AddPrefabPostInit("piggyback", fridgebackpack)
+	AddPrefabPostInit("krampus_sack", fridgebackpack)
+	AddPrefabPostInit("thatchpack", fridgebackpack)
+	AddPrefabPostInit("mailpack", fridgebackpack)
+	AddPrefabPostInit("piratepack", fridgebackpack)
+end
+
+--backpack --背包1
+--icepack --保鲜包1
+--krampus_sack --小偷包
+--mailpack --邮箱包1
+--piggyback --小猪包1
+--piratepack --海盗背包1
+--seasack --海上保鲜袋1
+--spicepack --厨师包1
+--thatchpack --编织袋1
+	
+-- 冰箱保鲜程度
+TUNING.PERISH_FRIDGE_MULT = GetModConfigData("icebox_freeze")
+
+---------------------------------------------------------------
+------------------------------移动速度-------------------------
+---------------------------------------------------------------
+
+-- 定义与读取移动速度功能
+local WalkRun = GetModConfigData("WalkRun")
+-- 移动刷掉开关
+if WalkRun == true then
+-- 移动速度修改
+TUNING.WILSON_WALK_SPEED = GetModConfigData("walk_speed")
+TUNING.WILSON_RUN_SPEED = GetModConfigData("run_speed")
+end
+
+---------------------------------------------------------------
+------------------------------快速采集-------------------------
+---------------------------------------------------------------
+
+-- 快速采集函数
+local Quick_Pick = GetModConfigData("MOD_QuickPick")
+
+if Quick_Pick == true then
+	function MOD_QuickPick(inst)
+		if inst.components.pickable then
+		inst.components.pickable.quickpick = true
+		end
+	end
+end
+
+--通过API接口使下列植物变成快速采集
+AddPrefabPostInit("grass", MOD_QuickPick)
+AddPrefabPostInit("sapling", MOD_QuickPick)
+AddPrefabPostInit("reeds", MOD_QuickPick)
+AddPrefabPostInit("berrybush", MOD_QuickPick)
+AddPrefabPostInit("berrybush2", MOD_QuickPick)
+AddPrefabPostInit("cactus", MOD_QuickPick)
+AddPrefabPostInit("cave_fern", MOD_QuickPick)
+AddPrefabPostInit("red_mushroom", MOD_QuickPick)
+AddPrefabPostInit("green_mushroom", MOD_QuickPick)
+AddPrefabPostInit("blue_mushroom", MOD_QuickPick)
+AddPrefabPostInit("cave_banana_tree", MOD_QuickPick)
+AddPrefabPostInit("lichen", MOD_QuickPick)
+AddPrefabPostInit("marsh_bush", MOD_QuickPick)
+AddPrefabPostInit("flower_cave", MOD_QuickPick)
+AddPrefabPostInit("flower_cave_double", MOD_QuickPick)
+AddPrefabPostInit("flower_cave_triple", MOD_QuickPick)
+AddPrefabPostInit("seaweed_planted", MOD_QuickPick)
+AddPrefabPostInit("limpetrock", MOD_QuickPick)
+AddPrefabPostInit("mussel_farm", MOD_QuickPick)
+AddPrefabPostInit("grass_water", MOD_QuickPick)
+AddPrefabPostInit("sweet_potato_planted", MOD_QuickPick)
+AddPrefabPostInit("coffeebush", MOD_QuickPick)
+AddPrefabPostInit("seashell_beached", MOD_QuickPick)
+AddPrefabPostInit("tumbleweed", MOD_QuickPick) --风滚草
+
+---------------------------------------------------------------
+------------------------------自动回血-------------------------
+---------------------------------------------------------------
+
+-- 定义与读取血量回复功能
+local healthregen = GetModConfigData("healthregen")
+-- 增加血量回复开关
+if healthregen == true then
+AddPlayerPostInit(function(inst)
+    -- 在玩家实体初始化后执行
+    if inst.components.health then
+        -- 参数1: 每秒回血量
+        -- 参数2: 回血间隔时间
+        inst.components.health:StartRegen(GetModConfigData("regen_health"),GetModConfigData("regen_time"))
+        -- 可选：设置最大生命值为威尔逊的标准值
+        --inst.components.health:SetMaxHealth(TUNING.WILSON_HEALTH)
+    end
+end)
+end
+
+--待添加小偷背包保鲜功能，牙齿陷阱自动重置功能，移动速度也添加一个开关，女武神吃素等相关人物修改，人物自带格子也能保鲜等，
+
+------------女武神吃素----------------------------
+local wathgrithr_vegetarian = GetModConfigData("wathgrithr_vegetarian")
+if wathgrithr_vegetarian == true then
+    AddPlayerPostInit(function(inst)
+        if inst.prefab == "wathgrithr" and inst.components.eater then
+            inst.components.eater:SetOmnivore()
+        end
+    end)
+end
