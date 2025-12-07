576ab7443530337ad628d6380fb018fceba95ad1
DevilWL <qq422909800@163.com>
2025-12-07 16:03:11 +0800

    五格装备栏调整

---
+++ 已优化/ds0074-五格装备栏（修复版）/modinfo.lua
@@ -0,0 +1,20 @@
+
+name = "五格装备栏"
+
+description = "额外增加背包栏与护身符栏\n允许玩家同时装备护甲与背包等装备"
+
+author = "Devil"
+
+version = "1.0.0"
+
+api_version = 6
+priority = 1
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+
+icon_atlas = "BackpackAmulet.xml"
+icon = "BackpackAmulet.tex"
+
+forumthread = ""
